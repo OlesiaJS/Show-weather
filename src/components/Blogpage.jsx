0e90121291fc1d1a8806688513228da988fe316d
OlesiaJS <Lessia92@i.ua>
2022-10-27 20:29:26 +0300

    withmyhook

--- src/components/Blogpage.jsx
+++ src/components/Blogpage.jsx
@@ -1,15 +1,13 @@
 import "../components/style.css";
-import useQuote from "../hooks/useCreatequote";
-
+// import useQuote from "../hooks/useCreatequote";
+import useQuote from "olesiafetchreacthook";
 function Blogpage() {
     const {isLoading, error, quote, author} = useQuote()
      console.log('quote:', quote); 
-
       return (
         <section className="weather-search quote">
                 <div className="news-info text-20"> {isLoading && <p>Loading...</p>}
                     {error && <p>Oops, no data</p>}<p>{quote}</p><p className="text-12-grey create-sign text-20"> {author}</p></div>
-            
         </section>
     );
 }
